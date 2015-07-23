b34f8bd6a13dbbe408c4edff7f383cf080e6e0cf
Jhonatan Oliveira <jhonatanoliveira@gmail.com>
2015-07-23 14:18:15 -0600

    Initial code structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="darwiniannetworks",
+    version="0.1",
+    description="A Python library for Darwinian networks modelling and exact inference",
+    author="Jhonatan Oliveira",
+    author_email="jhonatanoliveira@gmail.com",
+    packages=["darwiniannetworks"],
+    url="www2.cs.uregina.ca/~desouzjh/"
+)
